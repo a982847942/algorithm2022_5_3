31c2ce2f310766fa96ab7624c42a46d3e000e9d2
zk <982847942@qq.com>
2022-05-14 20:29:24 +0800

    UnionSet

---
+++ src/main/java/swu/zk/unionset/FindCircleNum.java
@@ -0,0 +1,142 @@
+package swu.zk.unionset;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @Classname FindCircleNum
+ * @Description https://leetcode.cn/problems/number-of-provinces/
+ * @Date 2022/5/14 10:31
+ * @Created by brain
+ */
+public class FindCircleNum {
+
+    /**
+     * 深度优先搜索
+     *
+     * @param isConnected
+     * @return
+     */
+    public int findCircleNum(int[][] isConnected) {
+        if (isConnected == null || isConnected.length == 0 || isConnected[0].length == 0) return 0;
+        boolean isVisited[] = new boolean[isConnected.length];
+        Integer count = 0;
+        for (int i = 0; i < isVisited.length; i++) {
+            if (!isVisited[i]) {
+                count++;
+                dfs(i, isConnected, isVisited);
+            }
+        }
+        return count;
+    }
+
+    private void dfs(int i, int[][] isConnected, boolean[] isVisited) {
+        isVisited[i] = true;
+        for (int j = 0; j < isConnected.length; j++) {
+            if (isConnected[i][j] == 1 && !isVisited[j]) {
+                dfs(j, isConnected, isVisited);
+            }
+        }
+    }
+
+    /**
+     * BFS
+     * @param isConnected
+     * @return
+     */
+    public int findCircleNum2(int[][] isConnected) {
+        if (isConnected == null || isConnected.length == 0 || isConnected[0].length == 0) return 0;
+        boolean isVisited[] = new boolean[isConnected.length];
+        Queue<Integer> queue = new LinkedList<>();
+        int result = 0;
+        for (int i = 0; i < isConnected.length; i++) {
+            if (!isVisited[i]) {
+                result++;
+                isVisited[i] = true;
+                queue.add(i);
+                while (!queue.isEmpty()) {
+                    Integer node = queue.poll();
+                    for (int j = 0; j < isConnected.length; j++) {
+                        if (isConnected[i][j] == 1 && !isVisited[j]) {
+                            queue.offer(j);
+                            isVisited[j] = true;
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 并查集
+     * @param isConnected
+     * @return
+     */
+    public int findCircleNum3(int[][] isConnected) {
+        Union union = new Union(isConnected.length);
+        for (int i = 0; i < isConnected.length; i++) {
+            for (int j = i + 1; j < isConnected.length; j++) {
+                if (isConnected[i][j] == 1){
+                    union.union(i,j);
+                }
+            }
+        }
+        return union.sets;
+    }
+
+    private class Union{
+        int[] help;
+        int[] size;
+        int[] parent;
+        int sets;
+
+        public Union(int n) {
+            sets = n;
+            help = new int[n];
+            size = new int[n];
+            parent = new int[n];
+            for (int i = 0; i < n; i++) {
+                size[i] = i;
+                parent[i] = i;
+            }
+        }
+
+        public boolean isSameSet(int a,int b){
+            return findParent(a) == findParent(b);
+        }
+
+        private Integer findParent(int node) {
+            Integer hi = 0;
+            while (node != parent[node]){
+                help[hi++] = node;
+                node = parent[node];
+            }
+            hi--;
+            while (hi >= 0){
+              parent[help[hi--]]  = node;
+            }
+            return node;
+        }
+
+        public void union(int a,int b){
+            Integer aParent = findParent(a);
+            Integer bParent = findParent(b);
+            if (aParent != bParent){
+                int aSize = size[a];
+                int bSize = size[b];
+                Integer bigNode = aSize >= bSize ? aParent : bParent;
+                Integer smallNode = bigNode == aParent ? bParent : aParent;
+                size[bigNode] = aSize + bSize;
+                size[smallNode] = 0;
+                sets--;
+            }
+        }
+
+        public Integer sets(){
+            return sets;
+        }
+    }
+
+}
--- src/main/java/swu/zk/unionset/UnionSet.java
+++ src/main/java/swu/zk/unionset/UnionSet.java
@@ -1,10 +1,91 @@
 package swu.zk.unionset;
 
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @Classname UnionSet
  * @Description 并查集
+ * 并查集是一种类似于树的结构
+ * 实现过程中添加了一些优化
+ * 1.路径压缩
+ * 2.按秩排序
  * @Date 2022/5/11 21:56
  * @Created by brain
  */
 public class UnionSet {
+
+
+    private static class Node<V>{
+        V value;
+
+        public Node(V value) {
+            this.value = value;
+        }
+    }
+
+    private static class Union<V>{
+        HashMap<V,Node<V>> nodes;
+        HashMap<Node<V>,Integer> sizeMap;
+        HashMap<Node<V>,Node<V>> parent;
+
+        public Union(List<V> list) {
+            nodes = new HashMap<>();
+            sizeMap = new HashMap<>();
+            parent = new HashMap<>();
+            for (V value : list) {
+                Node<V> node = new Node<>(value);
+                nodes.put(value,node);
+                parent.put(node,node);
+                sizeMap.put(node,1);
+            }
+        }
+
+        public boolean isSameSet(V node1,V node2){
+            Node<V> cur1 = nodes.get(node1);
+            Node<V> cur2 = nodes.get(node2);
+            if (cur1 == null || cur2 == null) return false;
+            return findParent(cur1) == findParent(cur2);
+        }
+
+        private Node<V> findParent(Node<V> node) {
+            //路径压缩  将树变的扁平
+            Deque<Node<V>> deque = new LinkedList<>();
+            while (node != parent.get(node)){
+                deque.push(node);
+                node = parent.get(node);
+            }
+
+            while (!deque.isEmpty()){
+                parent.put(deque.poll(),node);
+            }
+            return node;
+        }
+
+        public void union(V node1,V node2){
+            Node<V> cur1 = nodes.get(node1);
+            Node<V> cur2 = nodes.get(node2);
+            if (cur1 == null || cur2 == null) return;
+            Node<V> parent1 = findParent(cur1);
+            Node<V> parent2 = findParent(cur2);
+            if (parent1 != parent2){
+                Integer size1 = sizeMap.get(parent1);
+                Integer size2 = sizeMap.get(parent2);
+                Node<V> bigNode = size1 >= size2 ? cur1 : cur2;
+                Node<V> smallNode = bigNode == cur1 ? cur2 : cur1;
+                //按秩排序
+                parent.put(smallNode,bigNode);
+                sizeMap.put(bigNode,size1 + size2);
+                sizeMap.remove(smallNode);
+            }
+        }
+
+        public Integer sets(){
+            return sizeMap.size();
+        }
+    }
+
+
 }
